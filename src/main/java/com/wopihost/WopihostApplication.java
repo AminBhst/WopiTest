ab170515becd6dfc37b2c97bbd033a977874d43b
beheshti <beheshti@saadafzar.com>
2022-09-11 12:07:54 +0430

    first commit

---
+++ src/main/java/com/wopihost/WopihostApplication.java
@@ -0,0 +1,16 @@
+package com.wopihost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ethendev
+ * @date 2017/4/15
+ */
+@SpringBootApplication
+public class WopihostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WopihostApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/wopwopi/WopwopiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wopwopi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WopwopiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
